5fbd83aa5b6e067a54d3c4032a3f02f746a41467
rcbxd <rbcxdd@gmail.com>
2022-08-03 16:20:28 -0500

    fixed cards on tablets

--- src/components/cards.tsx
+++ src/components/cards.tsx
@@ -7,7 +7,7 @@ export const MachineCards = () => {
   return (
     <div className="flex items-center justify-center flex-col md:flex-row">
       {machines.map((props) => (
-        <div key={props.name} className="w-[360px] mr-0 md:mr-12 last:mr-0 mb-5 md:mb-0 last:mb-0">
+        <div key={props.name} className="w-[360px] md:w-[260px] lg:w-[360px] mr-0 md:mr-2 lg:mr-12 last:mr-0 mb-5 md:mb-0 last:mb-0">
           <MachineCard {...props} />
         </div>
       ))}
