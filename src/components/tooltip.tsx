04724a2dd8dfe5e58df758ca25dc6872a8348c48
rcbxd <rbcxdd@gmail.com>
2022-08-03 16:16:57 -0500

    fixed the tooltip going out of page on mobile devices

--- src/components/tooltip.tsx
+++ src/components/tooltip.tsx
@@ -3,7 +3,7 @@ import React, {useState} from "react";
 export default function ToolTip({ desc, isActive }: { desc: string, isActive: boolean }) {
   if (isActive) {
     return (
-      <div className="absolute z-50 bg-white ml-[150px] mt-[50px] w-[300px] p-4 rounded-lg drop-shadow-md">
+      <div className="absolute z-50 bg-white md:ml-[150px] mt-[40px] md:mt-[50px] w-[300px] p-4 rounded-lg drop-shadow-md">
         <p className="text-sm">{desc}</p>
       </div>
     )
